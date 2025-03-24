87df0d2c771be42165429fe1b29dd68cf0a0e654
Carl-Tabuso <carlftabuso@iskolarngbayan.pup.edu.ph>
2025-03-24 18:04:25 +0800

    chore: Create helper for formulating a response payload

---
+++ app/Traits/HttpResponse.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Traits;
+
+use Illuminate\Http\JsonResponse;
+use Symfony\Component\HttpFoundation\Response;
+
+trait HttpResponse
+{
+    public function createResponse($data = null, $message = null, $status): JsonResponse
+    {
+        $payload = ['status' => $status];
+
+        if ($data) {
+            $payload['data'] = $data;
+        }
+
+        if ($message) {
+            $payload = array_merge(['message' => $message], $payload);
+        }
+
+        return response()->json($payload, $status);
+    }
+
+    public function success($data = null, $message = null, $status = Response::HTTP_OK): JsonResponse
+    {
+        return $this->createResponse($data, $message, $status);
+    }
+
+    public function error($data = null, $message = null, $status = Response::HTTP_INTERNAL_SERVER_ERROR): JsonResponse 
+    {
+        return $this->createResponse($data, $message, $status);
+    }
+}
