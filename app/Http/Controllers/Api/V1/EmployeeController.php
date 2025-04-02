b6e36684b70001691a81f99843665ab85f37b7ec
JBCervantes <johnbryancervantes.study@gmail.com>
2025-04-03 00:45:15 +0800

    feat: add get single, get all, and update employee methods in EmployeeController

--- app/Http/Controllers/Api/V1/EmployeeController.php
+++ app/Http/Controllers/Api/V1/EmployeeController.php
@@ -3,18 +3,32 @@
 namespace App\Http\Controllers\Api\V1;
 
 use App\Http\Resources\V1\EmployeeResource;
+use App\Http\Resources\V1\EmployeeCollection;
+use Exception;
+use App\Services\V1\EmployeeService;
+use Illuminate\Http\JsonResponse;
+use App\Http\Requests\V1\UpdateEmployeeRequest;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 use Illuminate\Support\Facades\Validator;
 use App\Models\Employee;
+use Symfony\Component\HttpFoundation\Response;
+use App\Traits\HttpResponse;
 
 class EmployeeController extends Controller
 {
-    public function __construct() {}
+    use HttpResponse;
 
-    public function index()
+    public function __construct(protected EmployeeService $employeeService) {}
+
+    public function index(Request $request): JsonResponse|EmployeeCollection
     {
-        //
+        try {
+            $employees = $this->employeeService->getEmployees($request);
+            return EmployeeCollection::make($employees);
+        } catch (Exception $e) {
+            return $this->error(message: $e->getMessage());
+        }
     }
 
 public function store(Request $request)
@@ -48,14 +62,33 @@ public function store(Request $request)
     }
 
 
-    public function show(mixed $id)
+    public function show(Request $request, string $id): JsonResponse|EmployeeResource
     {
-        //
+        try {
+            $employee = $this->employeeService->getEmployee($request, $id);
+            return $this->success(data: EmployeeResource::make($employee));
+        } catch (Exception $e) {
+            return $this->error(
+                message: $e->getMessage(), 
+                status: Response::HTTP_NOT_FOUND
+            );
+        }
     }
 
-    public function update(Request $request, int $id)
+    public function update(UpdateEmployeeRequest $request, string $id): JsonResponse|EmployeeResource
     {
-        //
+        try {
+            $updatedEmployee = $this->employeeService->updateEmployee($request->validated(), $id);
+            return $this->success(
+                data: EmployeeResource::make($updatedEmployee),
+                message: __('response.success.update', ['resource' => 'employee']),
+            );
+        } catch (Exception $e) {
+            return $this->error(
+                message: $e->getMessage(),
+                status: Response::HTTP_NOT_FOUND,
+            );
+        }
     }
 
     public function destroy(int $id)
