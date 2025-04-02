df5057113b83c72bd3c69a9d167664c987d6bed3
hatdogguldo <redondoblessed@gmail.com>
2025-04-02 10:28:40 +0800

    feat: Build Employee Dashboard Frontend

---
+++ app/Http/Livewire/EmpDashboard.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Livewire;
+
+use Livewire\Component;
+
+class EmpDashboard extends Component
+{
+    public function render()
+    {
+        return view('livewire.emp-dashboard');
+    }
+}
