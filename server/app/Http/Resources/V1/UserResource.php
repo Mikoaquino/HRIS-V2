03f9201e8731cf888a6acef41f4154a1275c5422
Carl-Tabuso <carlftabuso@iskolarngbayan.pup.edu.ph>
2025-04-15 23:41:29 +0800

    chore: Don't return `password` and `remember_token`

--- server/app/Http/Resources/V1/UserResource.php
+++ server/app/Http/Resources/V1/UserResource.php
@@ -12,8 +12,8 @@ class UserResource extends JsonResource
         return [
             'id' => $this->id,
             'email' => $this->email,
-            'password' => $this->password,
-            'remember_token' => $this->remember_token,
+            // 'password' => $this->password,
+            // 'remember_token' => $this->remember_token,
             'status' => $this->status,
             'created_at' => $this->created_at,
             'updated_at' => $this->updated_at,
