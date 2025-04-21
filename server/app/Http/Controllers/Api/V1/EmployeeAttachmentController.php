8a7e3e4c0e560086ab4699fe0608c492acfff452
Carl-Tabuso <carlftabuso@iskolarngbayan.pup.edu.ph>
2025-04-21 14:24:52 +0800

    feat: Create api endpoints for handling multiple file uploads

--- server/app/Http/Controllers/Api/V1/EmployeeAttachmentController.php
+++ server/app/Http/Controllers/Api/V1/EmployeeAttachmentController.php
@@ -3,45 +3,35 @@
 namespace App\Http\Controllers\Api\V1;
 
 use App\Traits\HttpResponse;
+use Illuminate\Http\JsonResponse;
 use App\Models\EmployeeAttachment;
 use App\Http\Controllers\Controller;
-use App\Http\Requests\V1\StoreEmployeeAttachmentRequest;
 use Illuminate\Support\Facades\Storage;
+use App\Services\V1\EmployeeAttachmentService;
 use Symfony\Component\HttpFoundation\Response;
-use App\Http\Resources\V1\EmployeeAttachmentCollection;
+use Illuminate\Http\Response as IlluminateHttpResponse;
+use App\Http\Requests\V1\StoreEmployeeAttachmentRequest;
 
 class EmployeeAttachmentController extends Controller
 {
     use HttpResponse;
 
-    public function store(StoreEmployeeAttachmentRequest $request)
+    public function __construct(protected EmployeeAttachmentService $service) {}
+
+    public function store(StoreEmployeeAttachmentRequest $request): JsonResponse
     {
-        $validated = (object) $request->validated();
-
-        $data = array_map(function ($attachment) use ($validated) {
-            $attachment->store('employees');
-
-            return [
-                'employee_id' => $validated->employee_id,
-                'client_name' => $attachment->getClientOriginalName(),
-                'hashed_name' => $attachment->hashName(),
-                'created_at' => now(),
-                'updated_at' => now(),
-            ];
-        }, $validated->attachments);
-
-        EmployeeAttachment::insert($data);
+        $responseData = $this->service->handleUploads($request->validated());
 
         return $this->success(
-            EmployeeAttachmentCollection::make(EmployeeAttachment::hydrate($data)),
+            $responseData,
             __('response.attachment.upload.success'),
             Response::HTTP_CREATED
         );
     }
 
-    public function show(EmployeeAttachment $attachment)
+    public function show(EmployeeAttachment $attachment): JsonResponse|IlluminateHttpResponse
     {
-        $content = Storage::get(sprintf("%s/%s", 'employees', $attachment->hashed_name));
+        $content = $this->service->getAttachment($attachment);
 
         if (! $content) {
             return $this->error(
@@ -53,30 +43,5 @@ class EmployeeAttachmentController extends Controller
         return response($content, Response::HTTP_FOUND, [
             'Content-Type' => Storage::mimeType($content),
         ]);
-    }
-
-    public function destroy(EmployeeAttachment $attachment)
-    {
-        if (! $attachment->trashed()) {
-            $attachment->delete();
-
-            return $this->success(message: __('response.attachment.delete.temporary.success', [
-                'attachment' => $attachment->client_name,
-            ]));
-        }
-
-        $isDeleted = Storage::delete(sprintf("%s/%s", 'employees', $attachment->hashed_name));
-
-        if (! $isDeleted) {
-            return $this->error(message: __('response.attachment.delete.permanent.error', [
-                'attachment' => $attachment->client_name
-            ]));
-        }
-
-        $attachment->forceDelete();
-
-        return $this->success(message: __('response.attachment.delete.permanent.success', [
-            'attachment' => $attachment->client_name,
-        ]));
     }
 }
