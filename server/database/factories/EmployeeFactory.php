66ba9313d8ede8279f970b39b096dda245b325ce
harley-ronquillo <harleyronquillo@gmail.com>
2025-04-21 18:06:08 +0800

    Modified EmployeeFactory to support new columns

--- server/database/factories/EmployeeFactory.php
+++ server/database/factories/EmployeeFactory.php
@@ -18,8 +18,16 @@ class EmployeeFactory extends Factory
     {
         return [
             'first_name' => fake()->firstName(),
-            'middle_name' => fake()->optional()->firstName(),
+            'middle_name' => fake()->randomElement([fake()->firstName(), null]),
             'last_name' => fake()->lastName(),
+            'suffix' => $this->faker->numberBetween(0,2) == 1 ? fake()->randomElement(['Jr.', 'Sr.']) : null,
+            'gender' => fake()->randomElement(['male', 'female']),
+            'birth_date' => fake()->date(),
+            'civil_status' => fake()->randomElement(['single', 'married', 'divorced', 'widowed']),
+            'nationality' => fake()->country(),
+            'religion' => fake()->randomElement(['Christianity', 'Islam', 'Hinduism', 'Buddhism']),
+            'contact_number' => $this->faker->regexify('09\d{9}'),
+            'employment_type_id' => fake()->randomElement([1, 2, 3, 4]), // Assuming you have employment types with IDs 1 to 4
         ];
     }
 }
