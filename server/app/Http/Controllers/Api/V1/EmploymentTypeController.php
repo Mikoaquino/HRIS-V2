969795a38aceb3ac316b05c1682192048a0f42cb
harley-ronquillo <harleyronquillo@gmail.com>
2025-04-23 15:41:50 +0800

    Removed unused imports

--- server/app/Http/Controllers/Api/V1/EmploymentTypeController.php
+++ server/app/Http/Controllers/Api/V1/EmploymentTypeController.php
@@ -2,12 +2,9 @@
 
 namespace App\Http\Controllers\Api\v1;
 
-use App\Http\Resources\v1\EmploymentTypeCollection;
-use App\Traits\HttpResponse;
-use Illuminate\Http\Request;
 use App\Models\EmploymentType;
 use App\Http\Controllers\Controller;
-use App\Http\Resources\V1\CityCollection;
+
 
 class EmploymentTypeController extends Controller
 {
