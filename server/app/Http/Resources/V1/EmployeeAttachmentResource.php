cc77c5bf489eadfedaefbfd74ffadd7ed21982ac
Carl-Tabuso <carlftabuso@iskolarngbayan.pup.edu.ph>
2025-04-24 10:54:23 +0800

    chore: Match keys to model attributes

--- server/app/Http/Resources/V1/EmployeeAttachmentResource.php
+++ server/app/Http/Resources/V1/EmployeeAttachmentResource.php
@@ -10,8 +10,8 @@ class EmployeeAttachmentResource extends JsonResource
     public function toArray(Request $request): array
     {
         return [
-            'client' => $this->client_name,
-            'path' => $this->hashed_name,
+            'client_name' => $this->client_name,
+            'hashed_name' => $this->hashed_name,
             'created_at' => $this->created_at,
             'updated_at' => $this->updated_at,
             'deleted_at' => $this->deleted_at,
