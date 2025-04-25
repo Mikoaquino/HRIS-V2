2343550de8b894d475bfd710f60f6d75aa985d23
Carl-Tabuso <carlftabuso@iskolarngbayan.pup.edu.ph>
2025-04-25 15:11:52 +0800

    chore: Resource collection for auth

--- server/app/Http/Resources/V1/ActivityResource.php
+++ server/app/Http/Resources/V1/ActivityResource.php
@@ -13,15 +13,11 @@ class ActivityResource extends JsonResource
             'id' => $this->id,
             'log_name' => $this->log_name,
             'description' => $this->description,
-            'subject_type' => $this->subject_type,
             'event' => $this->event,
-            'subject_id' => $this->subject_id,
-            'causer_type' => $this->causer_type,
-            'causer_id' => $this->causer_id,
-            'properties' => $this->properties,
             'batch_uuid' => $this->batch_uuid,
             'created_at' => $this->created_at,
             'updated_at' => $this->updated_at,
+            'causer' => EmployeeResource::make($this->causer),
         ];
     }
 }
