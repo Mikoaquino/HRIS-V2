9bcbb898a4dcfcfa0729285661c04794164175b9
Carl-Tabuso <carlftabuso@iskolarngbayan.pup.edu.ph>
2025-05-12 19:10:31 +0800

    chore: Receive and validate incoming request to `/employees` endpoint

--- server/app/Http/Requests/StoreEmployeeRequest.php
+++ server/app/Http/Requests/StoreEmployeeRequest.php
@@ -2,12 +2,7 @@
 
 namespace App\Http\Requests;
 
-use App\Enums\CivilStatus;
-use App\Enums\Gender;
-use Illuminate\Foundation\Http\FormRequest;
-use Illuminate\Validation\Rule;
-
-class StoreEmployeeRequest extends FormRequest
+class StoreEmployeeRequest extends EmployeeFormRequest
 {
     public function authorize(): bool
     {
@@ -16,23 +11,6 @@ class StoreEmployeeRequest extends FormRequest
 
     public function rules(): array
     {
-        return [
-            'first_name'         => ['required', 'string'],
-            'middle_name'        => ['nullable', 'string'],
-            'last_name'          => ['required', 'string'],
-            'suffix'             => ['nullable', 'string'],
-            'birth_date'         => ['required', 'date'],
-            'gender'             => ['required', Rule::in(Gender::getValues())],
-            'civil_status'       => ['required', Rule::in(CivilStatus::getValues())],
-            'nationality'        => ['required', 'string'],
-            'religion'           => ['required', 'string'],
-            'contact_number'     => ['required', 'string', 'max:11', 'min:11', 'regex:/^09\d{9}$/'],
-            'sss_id'             => ['required', 'numeric', 'digits:12'],
-            'tin_id'             => ['required', 'numeric', 'digits:12'],
-            'philhealth_id'      => ['required', 'numeric', 'digits:12'],
-            'pagibig_id'         => ['required', 'numeric', 'digits:12'],
-            'employment_type_id' => ['required', 'exists:employment_types,id'],
-            'job_position_id'    => ['required', 'exists:job_positions,id'],
-        ];
+        return $this->baseRules();
     }
 }
