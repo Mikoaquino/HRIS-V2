fd600271233a248e6756280768ceb58de373be56
Carl-Tabuso <carlftabuso@iskolarngbayan.pup.edu.ph>
2025-05-12 19:13:39 +0800

    chore: Handle multiple updates and inserts

--- server/app/Services/EmployeeEducationService.php
+++ server/app/Services/EmployeeEducationService.php
@@ -7,16 +7,35 @@ use Illuminate\Database\Eloquent\Collection;
 
 class EmployeeEducationService
 {
-    public function createEducations(array $validated): Collection
+    public function createEducation(array $validated): Collection
     {
         $data = array_map(fn ($education) => [
             'employee_id'  => $validated['employee_id'],
             'school'       => $education['school'],
             'degree'       => $education['degree'],
             'graduated_at' => $education['graduated_at'],
+            'created_at'   => now(),
+            'updated_at'   => now(),
         ], $validated['educations']);
 
         EmployeeEducation::insert($data);
+
+        return EmployeeEducation::hydrate($data);
+    }
+
+    public function updateEducation(array $validated): Collection
+    {
+        $data = array_map(fn ($education) => [
+            'id'           => $education['id'] ?? null,
+            'employee_id'  => $validated['employee_id'],
+            'school'       => $education['school'],
+            'degree'       => $education['degree'],
+            'graduated_at' => $education['graduated_at'],
+        ], $validated['educations']);
+
+        EmployeeEducation::upsert($data, 'id', [
+            'school', 'degree', 'graduated_at',
+        ]);
 
         return EmployeeEducation::hydrate($data);
     }
--- server/app/Services/EmployeeWorkExperienceService.php
+++ server/app/Services/EmployeeWorkExperienceService.php
@@ -16,9 +16,34 @@ class EmployeeWorkExperienceService
             'from'               => $workExperience['from'],
             'to'                 => $workExperience['to'],
             'reason_for_leaving' => $workExperience['reason_for_leaving'],
+            'created_at'         => now(),
+            'updated_at'         => now(),
         ], $validated['work_experiences']);
 
         EmployeeWorkExperience::insert($data);
+
+        return EmployeeWorkExperience::hydrate($data);
+    }
+
+    public function updateWorkExperiences(array $validated): Collection
+    {
+        $data = array_map(fn ($workExperience) => [
+            'id'                 => $workExperience['id'] ?? null,
+            'employee_id'        => $validated['employee_id'],
+            'previous_employer'  => $workExperience['previous_employer'],
+            'job_position'       => $workExperience['job_position'],
+            'from'               => $workExperience['from'],
+            'to'                 => $workExperience['to'],
+            'reason_for_leaving' => $workExperience['reason_for_leaving'],
+        ], $validated['work_experiences']);
+
+        EmployeeWorkExperience::upsert($data, 'id', [
+            'previous_employer',
+            'job_position',
+            'from',
+            'to',
+            'reason_for_leaving',
+        ]);
 
         return EmployeeWorkExperience::hydrate($data);
     }
