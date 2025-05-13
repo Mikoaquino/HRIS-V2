f2a162fe9625808c983decad5d823923b4a6537c
Carl-Tabuso <carlftabuso@iskolarngbayan.pup.edu.ph>
2025-05-13 11:16:03 +0800

    chore: Add `email_verified_at` in resoure schema

--- server/app/Http/Resources/UserResource.php
+++ server/app/Http/Resources/UserResource.php
@@ -10,14 +10,15 @@ class UserResource extends JsonResource
     public function toArray(Request $request): array
     {
         return [
-            'id'          => $this->id,
-            'email'       => $this->email,
-            'employee_id' => $this->employee_id,
-            'status'      => $this->status,
-            'created_at'  => $this->created_at,
-            'updated_at'  => $this->updated_at,
-            'deleted_at'  => $this->deleted_at,
-            'employee'    => EmployeeResource::make($this->whenLoaded('employee')),
+            'id'                => $this->id,
+            'email'             => $this->email,
+            'employee_id'       => $this->employee_id,
+            'status'            => $this->status,
+            'email_verified_at' => $this->whenNotNull($this->email_verified_at),
+            'created_at'        => $this->created_at,
+            'updated_at'        => $this->updated_at,
+            'deleted_at'        => $this->whenNotNull($this->deleted_at),
+            'employee'          => EmployeeResource::make($this->whenLoaded('employee')),
         ];
     }
 }
