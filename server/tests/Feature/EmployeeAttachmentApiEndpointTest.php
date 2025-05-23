5056a4f844122839e4d3636bacfd6105dc879da3
Carl-Tabuso <carlftabuso@iskolarngbayan.pup.edu.ph>
2025-05-23 17:22:12 +0800

    test: Implement file soft-delete and force delete both in table and local disk

--- server/tests/Feature/EmployeeAttachmentApiEndpointTest.php
+++ server/tests/Feature/EmployeeAttachmentApiEndpointTest.php
@@ -1,15 +1,8 @@
 <?php
 
 use App\Models\Employee;
-use App\Models\User;
 use Illuminate\Http\UploadedFile;
 use Symfony\Component\HttpFoundation\Response;
-
-beforeEach(function () {
-    $this->user = User::factory()->create();
-
-    $this->token = $this->user->createToken('access-token')->plainTextToken;
-});
 
 test('`POST:` Save a single file in the local disk', function () {
     $attachment = UploadedFile::fake()->create('document.pdf');
@@ -39,7 +32,8 @@ test('`POST:` Save a single file in the local disk', function () {
         ->assertJsonFragment([
             'client_name' => $attachment->getClientOriginalName(),
             'hashed_name' => $attachment->hashName(),
-        ]);
+        ])
+        ->assertJsonMissingPath('data.0.deleted_at');
 });
 
 test('`POST:` Save multiple files in the local disk', function () {
@@ -64,17 +58,85 @@ test('`POST:` Save multiple files in the local disk', function () {
 
     Storage::disk('local')->delete($hashedAttachments);
 
-    $response->assertCreated();
-})->wip();
+    $response
+        ->assertCreated()
+        ->assertExactJsonStructure(['message', 'status', 'data'])
+        ->assertJson([
+            'message' => __('response.attachment.upload.success'),
+            'status'  => Response::HTTP_CREATED,
+        ]);
 
-test('`GET:` Render the file and its contents', function () {
-    //
-})->todo();
+    expect($response->getData()->data)
+        ->toHaveCount(count($attachments))
+        ->each()->not()->toHaveKey('deleted_at');
+
+    foreach ($response->getData()->data as $key => $data) {
+        expect($data)->toMatchObject([
+            'client_name' => $attachments[$key]->getClientOriginalName(),
+            'hashed_name' => $attachments[$key]->hashName(),
+        ]);
+    }
+});
 
 test('`DELETE:` Flag a file resource as deleted', function () {
-    //
-})->todo();
+    $attachment = UploadedFile::fake()->create('document.pdf');
+
+    $response = $this->withHeaders([
+        'Authorization' => 'Bearer '.$this->token,
+    ])->postJson('/api/v1/attachments', [
+        'employee_id' => Employee::factory()->create()->id,
+        'attachments' => [$attachment],
+    ])->assertCreated();
+
+    $response = $this->withHeaders([
+        'Authorization' => 'Bearer '.$this->token,
+    ])->deleteJson('/api/v1/attachments/'.$attachment->hashName());
+
+    $hashedAttachment = 'employees/'.$attachment->hashName();
+
+    Storage::disk('local')->assertExists($hashedAttachment);
+
+    Storage::disk('local')->delete($hashedAttachment);
+
+    $response
+        ->assertOk()
+        ->assertExactJsonStructure(['message', 'status'])
+        ->assertJson([
+            'message' => __('response.attachment.delete.temporary.success', [
+                'attachment' => $attachment->getClientOriginalName(),
+            ]),
+            'status'  => Response::HTTP_OK,
+        ]);
+});
 
 test('`DELETE:` Permanently delete a file in the local disk', function () {
-    //
-})->todo();
+    $attachment = UploadedFile::fake()->create('document.pdf');
+
+    $this->withHeaders([
+        'Authorization' => 'Bearer '.$this->token,
+    ])->postJson('/api/v1/attachments', [
+        'employee_id' => Employee::factory()->create()->id,
+        'attachments' => [$attachment],
+    ])->assertCreated();
+
+    $this->withHeaders([
+        'Authorization' => 'Bearer '.$this->token,
+    ])->deleteJson('/api/v1/attachments/'.$attachment->hashName())
+        ->assertOk();
+
+    $response = $this->withHeaders([
+        'Authorization' => 'Bearer '.$this->token,
+    ])->deleteJson('/api/v1/attachments/'.$attachment->hashName());
+
+    Storage::disk('local')->assertMissing('employees/'.$attachment->hashName());
+
+    $response
+        ->assertOk()
+        ->assertExactJsonStructure(['message', 'status'])
+        ->assertJson([
+            'message' => __('response.attachment.delete.permanent.success', [
+                'attachment' => $attachment->getClientOriginalName(),
+            ]),
+            'status'  => Response::HTTP_OK,
+        ]);
+});
