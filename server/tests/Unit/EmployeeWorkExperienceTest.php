bb64184d77869c592f67799dc3b88588bc375bd9
Carl-Tabuso <carlftabuso@iskolarngbayan.pup.edu.ph>
2025-05-26 22:16:46 +0800

    test: Implement update and multiple add in work experiences

--- server/tests/Unit/EmployeeWorkExperienceTest.php
+++ server/tests/Unit/EmployeeWorkExperienceTest.php
@@ -42,5 +42,56 @@ test('can create multiple employee work experience', function () {
 });
 
 test('can simultaneously update existing and add multiple employee work experience', function () {
-    //
-})->todo();
+    $employee = Employee::factory()->create();
+
+    for ($i = 0; $i < 5; $i++) {
+        $validated[] = [
+            'employee_id'        => $employee->id,
+            'previous_employer'  => fake()->company(),
+            'job_position'       => fake()->sentence(),
+            'from'               => fake()->date(),
+            'to'                 => fake()->date(),
+            'reason_for_leaving' => fake()->sentence(),
+        ];
+    }
+
+    $workExpCollection = $employee->workExperiences()->createMany($validated);
+
+    $updateData['work_experiences'] = $workExpCollection->map(fn ($workExp) => [
+        'id'                 => $workExp->id,
+        'employee_id'        => $employee->id,
+        'previous_employer'  => fake()->company(),
+        'job_position'       => fake()->sentence(),
+        'from'               => fake()->date(),
+        'to'                 => fake()->date(),
+        'reason_for_leaving' => fake()->sentence(),
+    ])->toArray();
+
+    $updateData['employee_id'] = $employee->id;
+
+    for ($i = 0; $i < 2; $i++) {
+        $updateData['work_experiences'][] = [
+            'employee_id'        => $employee->id,
+            'previous_employer'  => fake()->company(),
+            'job_position'       => fake()->sentence(),
+            'from'               => fake()->date(),
+            'to'                 => fake()->date(),
+            'reason_for_leaving' => fake()->sentence(),
+        ];
+    }
+
+    $this->service->updateWorkExperiences($updateData);
+
+    expect($employee->workExperiences->count())->toBe(7);
+
+    foreach ($updateData['work_experiences'] as $workExp) {
+        $this->assertDatabaseHas('employee_work_experiences', [
+            'employee_id'             => $updateData['employee_id'],
+            'previous_employer'       => $workExp['previous_employer'],
+            'job_position'            => $workExp['job_position'],
+            'from'                    => $workExp['from'],
+            'to'                      => $workExp['to'],
+            'reason_for_leaving'      => $workExp['reason_for_leaving'],
+        ]);
+    }
+});
