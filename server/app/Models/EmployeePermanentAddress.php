cfb7ebb997c62118de38869012993f3389347a5e
Carl-Tabuso <carlftabuso@iskolarngbayan.pup.edu.ph>
2025-05-26 22:15:53 +0800

    style: Apply pint

--- server/app/Models/EmployeePermanentAddress.php
+++ server/app/Models/EmployeePermanentAddress.php
@@ -9,7 +9,7 @@ use Illuminate\Database\Eloquent\Relations\BelongsTo;
 class EmployeePermanentAddress extends Model
 {
     use HasFactory;
-    
+
     protected $guarded = [
         'id',
         'created_at',
