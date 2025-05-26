061edcd114bf8843bc9a5c7c79a09fc6e65e5c02
Carl-Tabuso <carlftabuso@iskolarngbayan.pup.edu.ph>
2025-05-26 22:16:15 +0800

    refactor

--- server/tests/Unit/EmployeeEducationTest.php
+++ server/tests/Unit/EmployeeEducationTest.php
@@ -47,30 +47,30 @@ test('can simultaneously update existing and add multiple employee education', f
 
     $educationCollection = $employee->educations()->createMany($validated);
 
-    $updatedValidated['educations'] = $educationCollection->map(fn ($education) => [
+    $updateData['educations'] = $educationCollection->map(fn ($education) => [
         'id'           => $education->id,
         'school'       => fake()->company(),
         'degree'       => fake()->sentence(),
         'graduated_at' => fake()->date(),
     ])->toArray();
 
-    $updatedValidated['employee_id'] = $employee->id;
+    $updateData['employee_id'] = $employee->id;
 
     for ($i = 0; $i < 2; $i++) {
-        $updatedValidated['educations'][] = [
+        $updateData['educations'][] = [
             'school'       => fake()->company(),
             'degree'       => fake()->sentence(),
             'graduated_at' => fake()->date(),
         ];
     }
 
-    $this->service->updateEducation($updatedValidated);
+    $this->service->updateEducation($updateData);
 
     expect($employee->educations->count())->toBe(5);
 
-    foreach ($updatedValidated['educations'] as $education) {
+    foreach ($updateData['educations'] as $education) {
         $this->assertDatabaseHas('employee_educations', [
-            'employee_id'  => $updatedValidated['employee_id'],
+            'employee_id'  => $updateData['employee_id'],
             'school'       => $education['school'],
             'degree'       => $education['degree'],
             'graduated_at' => $education['graduated_at'],
